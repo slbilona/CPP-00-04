852c39888a774a35a79d024cd44211ceb58f6f02
Ilona SELBONNE <ilonaselbonne@hotmail.fr>
2023-11-28 18:05:16 +0100

    28/11/2023 18h05

--- CPP-00/ex02/Account.cpp
+++ CPP-00/ex02/Account.cpp
@@ -4,6 +4,7 @@
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 int Account::getNbAccounts(void) {
 	return _nbAccounts;
@@ -11,8 +12,11 @@ int Account::getNbAccounts(void) {
 
 void Account::displayAccountsInfos()
 {
-	std::cout << "[date] \n";
-	[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+	std::cout << "[date] accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+	for (int i = 0; i < 8; i++)
+	{
+		std::cout << "[date] index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+	}
 }
 
 void Account::makeDeposit(int deposit)
@@ -28,13 +32,20 @@ Account::Account(int initial_deposit)
 	_amount = initial_deposit;
 	_accountIndex = _nbAccounts;
 	_nbAccounts++;
+	_totalAmount += initial_deposit;
 	std::cout << "[date] index:" << _accountIndex << ";amount:" << _amount << ";created\n";
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
 	if ((_amount - withdrawal) >= 0)
+	{
+		_totalNbWithdrawals++;
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
 		return true;
+	}
 	else
 		return false;
 }
