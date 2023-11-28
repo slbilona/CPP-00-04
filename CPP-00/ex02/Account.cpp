45aee75408c08a77f16bf78265e0112253fce2ec
Ilona SELBONNE <ilonaselbonne@hotmail.fr>
2023-11-28 17:15:36 +0100

    28/11/2023 17h15

--- CPP-00/ex02/Account.cpp
+++ CPP-00/ex02/Account.cpp
@@ -1,4 +1,32 @@
-int main()
-{
-    
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+
+int Account::getNbAccounts(void) {
+	return _nbAccounts;
 }
+
+void Account::displayAccountsInfos()
+{
+	std::cout << "test\n";
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_amount += deposit;
+	_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
+
+Account::Account(int initial_deposit)
+{
+	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+}
+
+bool Account::makeWithdrawal
