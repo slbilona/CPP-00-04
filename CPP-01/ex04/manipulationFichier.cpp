54d0f9aef79b0934886df52a29a54ea775e7e633
Ilona <ilonaselbonne@hotmail.fr>
2023-12-06 14:54:58 +0100

    06/12/2023 14h54

--- CPP-01/ex04/manipulationFichier.cpp
+++ CPP-01/ex04/manipulationFichier.cpp
@@ -9,8 +9,22 @@ int ouvertureFichier(const std::string& monFichier, const std::string& s1, const
         std::cerr << "Erreur lors de l'ouverture du fichier." << std::endl;
         return 1;
     }
-	std::cout << s1 << " " << s2 << std::endl;
-	    // Fermer le fichier
+
+    //creer le nouveau fichier
+    std::string nomNouvFichier = monFichier + ".replace";
+	std::ofstream nouveauFichier(nomNouvFichier);
+
+    // Vérification si la création du fichier a réussi
+    if (!nouveauFichier.is_open()) {
+        std::cerr << "Erreur lors de la création du nouveauFichier." << std::endl;
+        return 1; // Quitter le programme avec un code d'erreur
+    }
+
+    // Écrire du texte dans le fichier
+    nouveauFichier << "Contenu du nouveau fichier." << std::endl;
+
+    // Fermer le fichier
+    nouveauFichier.close();
     fichier.close();
 	return (0);
 }
