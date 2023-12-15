df69d229434d3fb05218d1896171538c4f86fe9b
Ilona SELBONNE <ilonaselbonne@hotmail.fr>
2023-12-15 17:49:58 +0100

    15/12/2023 17h49

--- CPP-03/ex03/DiamondTrap.cpp
+++ CPP-03/ex03/DiamondTrap.cpp
@@ -1,9 +1,38 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(/* args */)
+DiamondTrap::DiamondTrap() : ClapTrap()
 {
+    std::cout << "DiamondTrap Default Constructor called" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name), name(name)
+{
+    std::cout << "DiamondTrap Constructor called" << std::endl;
+	hitPoints = 100;
+    energyPoints = 100;
+    attackDamage = 30;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& autre) : ClapTrap(autre)
+{
+    std::cout << "DiamondTrap Copy Constructor called" << std::endl;
+	*this = autre;
+}
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap& autre)
+{
+	if (this != &autre)
+	{
+		this->name = autre.name;
+		this->hitPoints = autre.hitPoints;
+		this->energyPoints = autre.energyPoints;
+		this->attackDamage = autre.attackDamage;
+        std::cout << "DiamondTrap Assignment Operator called" << std::endl;
+	}
+	return *this;
 }
 
 DiamondTrap::~DiamondTrap()
 {
+	std::cout << "DiamondTrap Destructor called" << std::endl;
 }
--- CPP-03/ex03/DiamondTrap.hpp
+++ CPP-03/ex03/DiamondTrap.hpp
@@ -4,15 +4,17 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
-class DiamondTrap
+class DiamondTrap : public FragTrap, public ScavTrap
 {
 private:
     std::string name;
 public:
     DiamondTrap();
+    DiamondTrap(std::string name);
     DiamondTrap(const DiamondTrap& autre);
     DiamondTrap& operator=(const DiamondTrap& autre);
     ~DiamondTrap();
+    ClapTrap::name(std::string name_clap_name);
 };
 
 #endif
