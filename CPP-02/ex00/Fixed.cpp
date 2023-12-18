2cd9f701364327f0658869cd39259e148174ff81
Ilona SELBONNE <ilonaselbonne@hotmail.fr>
2023-12-18 14:57:12 +0100

    18/12/2023 14h57

--- CPP-02/ex00/Fixed.cpp
+++ CPP-02/ex00/Fixed.cpp
@@ -4,44 +4,44 @@ const int Fixed::nbBits = 8;
 
 Fixed::Fixed() : valeur(0)
 {
-    std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& autre)
 {
-    std::cout << "Copy constructor called" << std::endl;
-    *this = autre;
+	valeur = autre.valeur;
+	std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(const Fixed& autre)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &autre)
+	if (this != &autre)
 	{
-        this->valeur = autre.valeur;
-    }
-    return *this;
+		this->valeur = autre.valeur;
+		std::cout << "Copy assignment operator called" << std::endl;
+	}
+	return *this;
 }
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits() const
 {
-    std::cout << "getRawBits member function called" << std::endl;
-    return this->valeur;
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->valeur;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << "setRawBits member function called" << std::endl;
-    this->valeur = raw;
+	std::cout << "setRawBits member function called" << std::endl;
+	this->valeur = raw;
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
 {
-    os << fixed.getRawBits();
-    return os;
+	os << fixed.getRawBits();
+	return os;
 }
