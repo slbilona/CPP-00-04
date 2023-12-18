5b306b37879eaf608cf335875d416145835b2774
Ilona SELBONNE <ilonaselbonne@hotmail.fr>
2023-12-18 15:25:21 +0100

    18/12/2023 15h25

--- CPP-02/ex02/main.cpp
+++ CPP-02/ex02/main.cpp
@@ -3,7 +3,7 @@
 int main( void )
 {
 	{
-		std::cout << "sujet :\n" << std::endl;
+		std::cout << "--------------- sujet ---------------" << std::endl;
 		Fixed a;
 		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 		std::cout << a << std::endl;
@@ -15,54 +15,55 @@ int main( void )
 		std::cout << Fixed::max( a, b ) << std::endl;
 	}
 	{
-		std::cout << "\n\nautres tests :\n" << std::endl;
+		std::cout << "\n--------------- Autres tests ---------------" << std::endl;
+
 		Fixed a(5.5f);
 		Fixed b(2.2f);
 
-		// Affichage des valeurs initiales
-		std::cout << "a: " << a << std::endl;
-		std::cout << "b: " << b << std::endl;
+		std::cout << "\nAffichage des valeurs initiales :" << std::endl;
+		std::cout << "a : " << a << std::endl;
+		std::cout << "b : " << b << std::endl;
 
-		// Utilisation des opérateurs arithmétiques
-		Fixed sum = a + b;
-		Fixed difference = a - b;
-		Fixed product = a * b;
+		std::cout << "\nOpérateurs arithmétiques :" << std::endl;
+		Fixed somme = a + b;
+		Fixed différence = a - b;
+		Fixed produit = a * b;
 		Fixed quotient = a / b;
 
-		std::cout << "Sum: " << sum << std::endl;
-		std::cout << "Difference: " << difference << std::endl;
-		std::cout << "Product: " << product << std::endl;
-		std::cout << "Quotient: " << quotient << std::endl;
+		std::cout << "Somme : " << somme << std::endl;
+		std::cout << "Différence : " << différence << std::endl;
+		std::cout << "Produit : " << produit << std::endl;
+		std::cout << "Quotient : " << quotient << std::endl;
 
-		// Utilisation des opérateurs de comparaison
-		std::cout << "a > b: " << (a > b) << std::endl;
-		std::cout << "a < b: " << (a < b) << std::endl;
-		std::cout << "a >= b: " << (a >= b) << std::endl;
-		std::cout << "a <= b: " << (a <= b) << std::endl;
-		std::cout << "a == b: " << (a == b) << std::endl;
-		std::cout << "a != b: " << (a != b) << std::endl;
+		std::cout << "\nOpérateurs de comparaison :" << std::endl;
+		std::cout << "a > b : " << (a > b) << std::endl;
+		std::cout << "a < b : " << (a < b) << std::endl;
+		std::cout << "a >= b : " << (a >= b) << std::endl;
+		std::cout << "a <= b : " << (a <= b) << std::endl;
+		std::cout << "a == b : " << (a == b) << std::endl;
+		std::cout << "a != b : " << (a != b) << std::endl;
 
-		// Utilisation des opérateurs d'incrémentation et décrémentation
+		std::cout << "\nOpérateurs d'incrémentation et décrémentation :" << std::endl;
 		++a;
-		std::cout << "a after pre-increment: " << a << std::endl;
+		std::cout << "a après pré-incrémentation : " << a << std::endl;
 
-		Fixed postIncrement = a++;
-		std::cout << "a after post-increment: " << a << std::endl;
-		std::cout << "postIncrement: " << postIncrement << std::endl;
+		Fixed postIncrément = a++;
+		std::cout << "a après post-incrémentation : " << a << std::endl;
+		std::cout << "postIncrément : " << postIncrément << std::endl;
 
 		--b;
-		std::cout << "b after pre-decrement: " << b << std::endl;
+		std::cout << "b après pré-décrémentation : " << b << std::endl;
 
-		Fixed postDecrement = b--;
-		std::cout << "b after post-decrement: " << b << std::endl;
-		std::cout << "postDecrement: " << postDecrement << std::endl;
+		Fixed postDécrément = b--;
+		std::cout << "b après post-décrémentation : " << b << std::endl;
+		std::cout << "postDécrément : " << postDécrément << std::endl;
 
-		// Utilisation des fonctions min et max
-		Fixed minResult = Fixed::min(a, b);
-		Fixed maxResult = Fixed::max(a, b);
+		std::cout << "\nMin et Max :" << std::endl;
+		Fixed résultatMin = Fixed::min(a, b);
+		Fixed résultatMax = Fixed::max(a, b);
 
-		std::cout << "Min: " << minResult << std::endl;
-		std::cout << "Max: " << maxResult << std::endl;
+		std::cout << "Min : " << résultatMin << std::endl;
+		std::cout << "Max : " << résultatMax << std::endl;
 	}
 	return 0;
 }
